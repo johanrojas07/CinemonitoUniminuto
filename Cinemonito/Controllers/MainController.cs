38ec628c9240cdafc4bf5d562f1fc1400bb925b0
Johan Esneyder Rojas Latorre <jerojas@ifxcorp.com>
2019-05-02 16:29:52 -0500

    Crud de empleados - Editar - Eliminar

--- Cinemonito/Controllers/MainController.cs
+++ Cinemonito/Controllers/MainController.cs
@@ -34,5 +34,51 @@ namespace Cinemonito.Controllers
                 return View(from Employee in db.Employee.ToList() select Employee);
             }
         }
+
+        public ActionResult delete(int id)
+        {
+            using (var db = new CinemonitoEntities())
+            {
+                var item = db.Employee.FirstOrDefault(x => x.Id == id);
+                if (item != null)
+                {
+                    db.Employee.Remove(item);
+                    db.SaveChanges();
+                }
+                var item2 = db.Employee.ToList();
+                return View("employees", item2);
+            }
+        }
+
+        public ActionResult editEmployee(int id)
+        {
+            using (var db = new CinemonitoEntities())
+            {
+                var item = db.Employee.Where(x => x.Id == id).First();
+                return View(item);
+            } 
+        }
+        [HttpPost]
+        public ActionResult editEmployee(Employee model)
+        {
+            using (var db = new CinemonitoEntities())
+            {
+                var item = db.Employee.Where(x => x.Id == model.Id).First();
+                item.Identification = model.Name;
+                item.Password = model.Password;
+                db.SaveChanges();
+                var item2 = db.Employee.ToList();
+                return View("employees", item2);
+            }
+                
+        }
+
+        public ActionResult createEmployee()
+        {
+            using (var db = new CinemonitoEntities())
+            {
+                return View();
+            }
+        }
     }
 }
