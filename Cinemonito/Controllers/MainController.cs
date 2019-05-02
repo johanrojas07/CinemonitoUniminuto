e0254037233629d86dc8cf5a1ef8c6faca29c76a
Johan Esneyder Rojas Latorre <jerojas@ifxcorp.com>
2019-05-02 09:34:56 -0500

    Inicio del proyecto

---
+++ Cinemonito/Controllers/MainController.cs
@@ -0,0 +1,38 @@
+﻿using Cinemonito.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinemonito.Controllers
+{
+    public class MainController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult buyTickets()
+        {
+            using (var db = new CinemonitoEntities())
+            {
+                return View(from Movie in db.Movie.ToList() select Movie);
+            }
+        }
+
+        public ActionResult buySnacks()
+        {
+            return View();
+        }
+
+        public ActionResult employees()
+        {
+            using (var db = new CinemonitoEntities())
+            {
+                return View(from Employee in db.Employee.ToList() select Employee);
+            }
+        }
+    }
+}
