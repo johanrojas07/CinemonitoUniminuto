ed5fdc631fb427bea6bbc067bbc512a083c5928e
Jair <jg.carranza25@gmail.com>
2019-05-03 09:44:50 -0500

    Ultimo Cambio

--- Cinemonito/Controllers/MainController.cs
+++ Cinemonito/Controllers/MainController.cs
@@ -80,5 +80,61 @@ namespace Cinemonito.Controllers
                 return View();
             }
         }
+
+        public ActionResult selectTicketsOption(int id)
+        {
+            using (var db = new CinemonitoEntities())
+            {
+                var datos = (from MoviesByRoom in db.MoviesByRoom
+                             join Room in db.Room on MoviesByRoom.IdRoom equals Room.Id
+                             join Headquarters in db.Headquarters on Room.IdHeadquarter equals Headquarters.Id
+                             where MoviesByRoom.IdMovie.Equals(id)
+                             select new
+                             {
+                                 name = Headquarters.Name,
+                                 id = Headquarters.Id,
+                                 address = Headquarters.Address,
+                                 idMovie = MoviesByRoom.IdMovie,
+                                 idRoom = MoviesByRoom.IdRoom
+                             }).ToList();
+                ViewBag.listMultiplex = datos;
+                return View();
+            }
+        }
+
+        public ActionResult selectRoom(Multiplex multiplex)
+        {
+            using (var db = new CinemonitoEntities())
+            {
+                var datos = (from MoviesByRoom in db.MoviesByRoom
+                             join Movie in db.Movie on MoviesByRoom.IdMovie equals Movie.Id
+                             where MoviesByRoom.IdMovie.Equals(multiplex.idMovie) && MoviesByRoom.IdMovie.Equals(multiplex.idRoom)
+                             select new
+                             {
+                                 nameMovie = Movie.Name,
+                                 horary = MoviesByRoom.Horary
+                             }).ToList();
+                multiplex.nameMovie = datos[0].nameMovie;
+                ViewBag.listMultiplex = datos;
+                return View();
+
+            }
+        }
+
+        public class Multiplex
+        {
+            public string name, address, nameMovie;
+            public int id, idMovie, idRoom;
+
+            public Multiplex()
+            {
+                this.nameMovie = null;
+                this.name = null;
+                this.address = null;
+                this.id = 0;
+                this.idMovie = 0;
+                this.idRoom = 0;
+            }
+        }
     }
 }
